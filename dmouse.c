95d4350e0101bd47e84d7b3e96db138946fae5ab
Ivan Tikhonov <kefeer@brokestream.com>
2014-06-29 01:14:40 +0600

    binary

--- dmouse.c
+++ dmouse.c
@@ -5,7 +5,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
+
 #include <X11/Xlib.h>
+#include <X11/extensions/XTest.h>
 
 
 static struct {
@@ -19,7 +21,9 @@ static struct {
 		int x,y;
 	} mouse;
 
-	int zoom;
+	struct {
+		int x,y;
+	} zoom;
 } data;
 
 void open_x(void);
@@ -29,7 +33,7 @@ void reset(void);
 void button(int);
 void read_info(void);
 void read_coord(void);
-int halve(char s);
+void halve(char s);
 void warp(void);
 void save_coord(void);
 
@@ -44,13 +48,13 @@ int main(int argc,char *argv[]) {
 
 	read_coord();
 
-	printf("coord %u %u, zoom %u\n",data.mouse.x,data.mouse.y,data.zoom);
+	printf("coord %u %u, zoom %u %u\n",data.mouse.x,data.mouse.y,data.zoom.x,data.zoom.y);
 
-	int need_warp=halve(argv[1][0]);
+	halve(argv[1][0]);
 
-	printf("warping to %u %u, zoom %u\n",data.mouse.x,data.mouse.y,data.zoom);
+	printf("warping to %u %u, zoom %u %u\n",data.mouse.x,data.mouse.y,data.zoom.x,data.zoom.y);
 
-	if(need_warp) warp();
+	warp();
 	save_coord();
 
 	close_x();
@@ -58,40 +62,35 @@ int main(int argc,char *argv[]) {
 
 #define MAX(x,y) ((x>y)?(x):(y))
 
-int halve(char q) {
-	int x,y;
+void halve(char q) {
+	int x=0,y=0;
 
 	switch(q) {
-	case 'q': x=-1; y=-1; break;
 	case 'w': x= 0; y=-1; break;
-	case 'e': x= 1; y=-1; break;
+	case 's': x= 0; y= 1; break;
 
 	case 'a': x=-1; y= 0; break;
-	case 's': x= 0; y= 0; break;
 	case 'd': x= 1; y= 0; break;
 
-	case 'z': x=-1; y= 1; break;
-	case 'x': x= 0; y= 1; break;
-	case 'c': x= 1; y= 1; break;
+	case 'e': reset(); return;
 
-	case 'R': reset(); return;
+	case 'q': button(Button1); return;
+	case 'c': button(Button2); return;
+	case 'x': button(Button3); return;
 
-	case 'l': button(Button1); return 0;
-	case 'm': button(Button2); return 0;
-	case 'r': button(Button3); return 0;
+	default: reset(); return;
 	}
 
-	int fx=MAX(1,data.screen.w/data.zoom);
-	int fy=MAX(1,data.screen.h/data.zoom);
+	int fx=MAX(1,data.screen.w/data.zoom.x);
+	int fy=MAX(1,data.screen.h/data.zoom.y);
 
 	printf("fx %u fy %u\n",fx,fy);
 
 	data.mouse.x=data.mouse.x+x*fx;
 	data.mouse.y=data.mouse.y+y*fy;
 
-	if(fx>1 || fy>1) data.zoom*=3;
-
-	return 1;
+	if(x && fx>1) data.zoom.x*=2;
+	if(y && fy>1) data.zoom.y*=2;
 }
 
 void reset(void) {
@@ -110,13 +109,15 @@ void read_coord(void) {
 	if(f==-1) {
 		data.mouse.x=data.screen.w/2;
 		data.mouse.y=data.screen.h/2;
-		data.zoom=3;
+		data.zoom.x=4;
+		data.zoom.y=4;
 		return;
 	}
 
 	read(f,&data.mouse.x,sizeof(data.mouse.x));
 	read(f,&data.mouse.y,sizeof(data.mouse.y));
-	read(f,&data.zoom,sizeof(data.zoom));
+	read(f,&data.zoom.x,sizeof(data.zoom.x));
+	read(f,&data.zoom.y,sizeof(data.zoom.y));
 
 	close(f);
 }
@@ -127,7 +128,8 @@ void save_coord(void) {
 
 	write(f,&data.mouse.x,sizeof(data.mouse.x));
 	write(f,&data.mouse.y,sizeof(data.mouse.y));
-	write(f,&data.zoom,sizeof(data.zoom));
+	write(f,&data.zoom.x,sizeof(data.zoom.x));
+	write(f,&data.zoom.y,sizeof(data.zoom.y));
 
 	close(f);
 
@@ -148,12 +150,12 @@ void close_x(void) {
 	XCloseDisplay(data.dpy);
 }
 
-void button(int b) {
+static void _button(int b,int press) {
 	XButtonEvent event;
 	
 	memset(&event,0,sizeof(event));
 	
-	event.type=ButtonPress;
+	event.type=press?ButtonPress:ButtonRelease;
 	event.button=b;
 	event.root=RootWindow(data.dpy,DefaultScreen(data.dpy));
 	event.same_screen=True;
@@ -169,16 +171,24 @@ void button(int b) {
 		event.window=event.subwindow;
 		XQueryPointer(data.dpy,event.window,&event.root,&event.subwindow,&event.x_root,&event.y_root,&event.x,&event.y,&event.state);
 	}
+
+	if(!press) {
+		switch(b) {
+		case Button1: event.state |= Button1MotionMask; break;
+		case Button2: event.state |= Button2MotionMask; break;
+		case Button3: event.state |= Button3MotionMask; break;
+		}
+	}
 	
 	XSendEvent(data.dpy,PointerWindow,True,ButtonPressMask,(XEvent *)&event);
 	XFlush(data.dpy);
 	
+	reset();
+}
+
+void button(int b) {
+	_button(b,1);
 	usleep(100000);
-	
-	event.type=ButtonRelease;
-	event.state=0x100;
-	
-	XSendEvent(data.dpy,PointerWindow,True,ButtonPressMask,(XEvent *)&event);
-	XFlush(data.dpy);
+	_button(b,0);
 }
 
